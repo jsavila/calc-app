f5db80ebd05b34fa60e610a9e2e9e32f79ff53fc
Juan Francisco Pineda <jfpinedap@unal.edu.co>
2021-06-18 23:26:17 -0500

    Feature: add buttons.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import react from "react"
+import React from "react"
 import reactDOM from "react-dom"
 import App from "./App"
 
 
-reactDOM.render( App(),
+reactDOM.render( <App />,
     document.getElementById("root")
 )
 
